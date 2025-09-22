fbac83e8571e120841e417c92c9705bd86b91c41
JobMutugi <creativejobu@gmail.com>
2025-09-22 19:33:44 +0300

    add changes made in app.py and models.py

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
 import os
 
@@ -18,6 +18,58 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 api = Api(app)
+
+
+class Restaurants(Resource):
+    def get(self):
+        restaurants = Restaurant.query.all()
+        return [r.to_dict(only=('id', 'name', 'address')) for r in restaurants], 200
+
+
+class RestaurantById(Resource):
+    def get(self, id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+        if not restaurant:
+            return {"error": "Restaurant not found"}, 404
+        return restaurant.to_dict(only=('id', 'name', 'address', 'restaurant_pizzas.id', 'restaurant_pizzas.pizza.id', 'restaurant_pizzas.pizza.name', 'restaurant_pizzas.pizza.ingredients', 'restaurant_pizzas.price', 'restaurant_pizzas.restaurant_id')), 200
+
+    def delete(self, id):
+        restaurant = Restaurant.query.filter_by(id=id).first()
+        if not restaurant:
+            return {"error": "Restaurant not found"}, 404
+        db.session.delete(restaurant)
+        db.session.commit()
+        return '', 204
+
+
+class Pizzas(Resource):
+    def get(self):
+        pizzas = Pizza.query.all()
+        return [p.to_dict(only=('id', 'name', 'ingredients')) for p in pizzas], 200
+
+
+class RestaurantPizzas(Resource):
+    def post(self):
+        data = request.get_json()
+        try:
+            rp = RestaurantPizza(
+                price=data['price'],
+                pizza_id=data['pizza_id'],
+                restaurant_id=data['restaurant_id']
+            )
+            db.session.add(rp)
+            db.session.commit()
+            return rp.to_dict(only=('id', 'pizza.id', 'pizza.name', 'pizza.ingredients', 'price', 'restaurant.id', 'restaurant.name', 'restaurant.address', 'pizza_id', 'restaurant_id')), 201
+        except ValueError:
+            return {"errors": ["validation errors"]}, 400
+        except:
+            return {"errors": ["validation errors"]}, 400
+
+
+api.add_resource(Restaurants, '/restaurants')
+api.add_resource(RestaurantById, '/restaurants/<int:id>')
+api.add_resource(Pizzas, '/pizzas')
+api.add_resource(RestaurantPizzas, '/restaurant_pizzas')
 
 
 @app.route("/")
